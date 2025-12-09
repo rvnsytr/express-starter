d5c61614008914613b9c2060f12066c75b0a060b
RvnSytR <ravenskytd17@gmail.com>
2025-12-09 14:52:29 +0700

    feat:  not found and error handler

--- src/core/middlewares.ts
+++ src/core/middlewares.ts
@@ -1,10 +1,13 @@
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler, RequestHandler } from "express";
 import z from "zod";
 import { apiResponseSchema } from "./schemas.zod";
 
-export type ApiResponse<T> = z.infer<typeof apiResponseSchema> & { data: T };
+export type ApiResponse<T> = z.infer<typeof apiResponseSchema> & {
+  data: T;
+  [k: string]: unknown;
+};
 
-export function apiResponse(_: Request, res: Response, next: NextFunction) {
+export const apiResponse: RequestHandler = (_req, res, next) => {
   res.api = <T>(payload: Partial<ApiResponse<T>>) => {
     const code = payload.code ?? 200;
     const success = code >= 200 && code < 300;
@@ -12,17 +15,33 @@ export function apiResponse(_: Request, res: Response, next: NextFunction) {
     const message =
       payload.message ??
       (success ? "Sukses" : "Terjadi kesalahan. Silakan coba lagi nanti.");
-    return res.status(code).json({ code, success, message, data });
+
+    return res.status(code).json({ code, success, message, data, ...payload });
   };
-
   next();
-}
+};
 
-export async function delayMiddleware(
-  _: Request,
-  __: Response,
-  next: NextFunction,
-) {
+export const delayHandler: RequestHandler = async (_req, _res, next) => {
   await new Promise((resolve) => setTimeout(resolve, 3 * 1000));
   next();
-}
+};
+
+export const notFoundHandler: RequestHandler = (_req, res) => {
+  const message = "Sumber daya yang diminta tidak ditemukan.";
+  return res.api({ code: 404, message });
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  const nodeEnv = process.env.NODE_ENV ?? "local";
+  const showError = nodeEnv === "local" || nodeEnv === "development";
+
+  const message = "Terjadi kesalahan pada server.";
+  const errorMessage =
+    err instanceof Error ? err.message : "Unknown error occurred";
+
+  const error = showError ? errorMessage : undefined;
+
+  console.error(err);
+  return res.api({ code: 500, message, error });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
-import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
-import { auth } from "./core/auth";
 import { appMeta } from "./core/constants";
-import { apiResponse } from "./core/middlewares";
+import { apiResponse, errorHandler, notFoundHandler } from "./core/middlewares";
+
+import authRoutes from "./modules/auth/routes";
 
 const app = express();
 const port = process.env.PORT ?? 8000;
 
-// app.use(delayMiddleware); // Testing Purpose
+// app.use(delayHandler); // Testing Purpose
 app.use(cors(appMeta.cors));
-app.all("/api/auth/*splat", toNodeHandler(auth));
+app.use(apiResponse);
+
+app.use("/api/auth", authRoutes);
 
 app.use(express.json());
-app.use(apiResponse);
 
 app.get("/api", (_, res) => res.api({ message: `Hello, ${appMeta.name}` }));
 
-app.all("*splat", (_, res) =>
-  res.api({ code: 404, message: "Sumber daya yang diminta tidak ditemukan." }),
-);
+app.all("*splat", notFoundHandler);
+app.use(errorHandler);
 
 app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`),
