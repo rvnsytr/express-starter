21e061faa8ae00a2ece7100f686136e72312b7bf
RvnSytR <ravenskytd17@gmail.com>
2026-01-13 09:11:45 +0700

    feat: db schema

--- src/core/schema.db.ts
+++ src/core/schema.db.ts
@@ -10,16 +10,22 @@ import {
   verificationTableSchema,
 } from "./schema.zod";
 
-type WithCreatedAt<T> = Omit<T, "created_at"> & {
+// type WithId<T> = Omit<T, "id"> & {
+//   id: ColumnType<string, never, never>;
+// };
+
+type WithAudit<T> = Omit<T, "updated_at" | "created_at"> & {
+  updated_at: ColumnType<Date, never, never>;
   created_at: ColumnType<Date, never, never>;
 };
 
 export type Database = {
-  user: WithCreatedAt<z.infer<typeof userTableSchema>>;
-  account: WithCreatedAt<z.infer<typeof accountTableSchema>>;
-  session: WithCreatedAt<z.infer<typeof sessionTableSchema>>;
-  verification: WithCreatedAt<z.infer<typeof verificationTableSchema>>;
-  storage: WithCreatedAt<z.infer<typeof storageTableSchema>>;
+  user: WithAudit<z.infer<typeof userTableSchema>>;
+  account: WithAudit<z.infer<typeof accountTableSchema>>;
+  session: WithAudit<z.infer<typeof sessionTableSchema>>;
+  verification: WithAudit<z.infer<typeof verificationTableSchema>>;
+
+  storage: WithAudit<z.infer<typeof storageTableSchema>>;
 };
 
 export type StorageTable = Selectable<Database["storage"]>;
